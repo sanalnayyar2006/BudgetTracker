a12788d06e930af75fb2abd45a35bd80791174d9
sanalnayyar <sanalsquad@gmail.com>
2025-05-05 23:03:16 +0530

    made the required components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import Navigation from './components/Navigation'
+import BudgetTracker from './components/BudgetTracker'
+import Debt from './components/Debt'
+import Courses from './components/Courses'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState('budget');
+
+function renderPage(){
+  if(page=='budget'){
+    return <BudgetTracker/>
+  }else if(page=='debt'){
+    return <Debt/>
+  }else if(page=='courses'){
+    return <Courses/>
+  }else{
+    return <BudgetTracker/>
+  }
+}
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='app-container'>
+        <Navigation currentPage={page} onChangePage={setPage}/>
+        <main className='main-content'>
+          {renderPage()}
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
